87aab874f3ebf40887558f103ff92c5bfe082b92
מיטל <ronni.ban@sisma.org.il>
2024-01-02 15:54:36 +0200

    my commit

--- app/src/main/java/com/example/ronibananoexe1/MainActivity.java
+++ app/src/main/java/com/example/ronibananoexe1/MainActivity.java
@@ -3,12 +3,21 @@ package com.example.ronibananoexe1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button bt;
+    //hgk
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bt=findViewById(R.id.button);
+    }
+
+    public void exe(View view) {
+        bt.setText("Oh,yea,i've been clicked");
     }
 }
